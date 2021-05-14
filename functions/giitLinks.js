ee8f3bc0bda34ca70d1743dc51a1bae5c80a11ef
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-05-14 14:49:24 +0500

    updates

---
+++ functions/giitLinks.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+require("dotenv").config();
+
+exports.handler = async (event) => {
+  const GET_LINKS = `
+    query{
+        allLinks{
+            data{
+            name
+            description
+            _id
+            archived
+            url
+            }
+        }
+    }`;
+  const { data } = await axios({
+    url: "https://graphql.fauna.com/graphql",
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.SECRET_KEY}`,
+    },
+    data: {
+      query: GET_LINKS,
+      variables: [],
+    },
+  });
+  console.log(data);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
